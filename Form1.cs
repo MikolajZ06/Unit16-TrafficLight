838f6df4f8b360dd2845ae4f9f8a9a7be597f04d
Mikolaj Zygmunt <438477@edu.cwa.ac.uk>
2024-04-19 09:41:23 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit16_TrafficLight
+{
+    enum LightColor
+    {
+        Green,
+        Yellow,
+        Red,
+        Off
+    }
+
+    class LightCycle
+    {
+        public LightColor[,] lightCycle = { { LightColor.Green, LightColor.Off, LightColor.Off },
+            { LightColor.Red, LightColor.Off, LightColor.Off } };
+
+        int cycleStage = 0;
+        LightColor currentStage = lightCycle[0];
+
+        public LightCycle(Side side)
+        {
+            if (side == Side.Right)
+                cycleStage = 1;
+        }
+
+        void NextStage()
+        {
+            if (currentStage + 1 == lightCycle.Length())
+            {
+                currentStage = lightCycle[0];
+                return;
+            }
+
+            currentStage = lightCycle[++cycleStage];
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var leftLight = new TrafficLight(Side.Left, e, Size.Width, Size.Height);
+            var rightLight = new TrafficLight(Side.Right, e, Size.Width, Size.Height);
+
+            leftLight.Draw(leftCycle.currentStage);
+            rightLight.Draw(rightCycle.currentStage);
+
+            LightCycle leftCycle = new LightCycle();
+            LightCycle rightCycle = new LightCycle();
+
+            leftCycle.NextStage();
+            rightCycle.NextStage();
+
+        }
+    }
+}
---
+++ TrafficLight.cs
@@ -0,0 +1,70 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+public enum Side { 
+    Left,
+    Right
+}
+
+public class TrafficLight
+{
+    public Side side;
+    public PaintEventArgs paintArgs;
+    public int windowHeight;
+    public int windowWidth;
+
+    public TrafficLight(Side side, PaintEventArgs e,int windowWidth, int windowHeight)
+    {
+        this.side = side;
+        this.paintArgs = e;
+        this.windowHeight = windowHeight;
+        this.windowWidth = windowWidth;
+    }
+
+    public void Draw()
+    {
+        DrawBox();
+        DrawCircles();
+    }
+
+    void DrawBox()
+    {
+        int rectHeight = 400;
+        int rectWidth = 200;
+        
+        int rectX = 50;
+        if (side == Side.Right)
+            rectX = windowWidth - rectWidth - 50;
+
+        int rectY = (windowHeight / 2) - rectHeight / 2;
+
+        var rect = new Rectangle(rectX, rectY, rectWidth, rectHeight);
+
+        var gp = new System.Drawing.Drawing2D.GraphicsPath();
+        gp.AddRectangle(rect);
+
+        paintArgs.Graphics.FillRegion(Brushes.Black, new Region(gp));
+    }
+
+    void DrawCircles()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var gp = new System.Drawing.Drawing2D.GraphicsPath();
+
+            int circleWidth = 100;
+            int circleX = 100;
+
+            if (side == Side.Right)
+                circleX = windowWidth - circleWidth - 100;
+            gp.AddEllipse(circleX, 80 + (i * 120), circleWidth, 100);
+            var region = new System.Drawing.Region(gp);
+
+            Graphics gr = paintArgs.Graphics;
+            gr.FillRegion(Brushes.DarkGray, region);
+
+            
+            
+        }
+    }
+}
